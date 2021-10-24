dd0faa94275ce69f73b112b37220bc0ce0aa3aed
your name <thienquan241096@gmail.com>
2021-10-24 14:43:25 +0700

    URL

--- routes/Frontend/web.php
+++ routes/Frontend/web.php
@@ -6,16 +6,15 @@ use App\Http\Controllers\Frontend\Services\ServicesController;
 use App\Http\Controllers\ContactController;
 
 Route::name('frontend.')->group(function () {
-    // router ( 'frontend.homepage.contact-form')
     Route::name('homepage.')->group(function () {
         Route::get('/', [HomepageController::class,'index'])->name('show');
     });
     
     Route::name('services.')->group(function () {
-        Route::get('/services', [ServicesController::class,'index'])->name('show');
+        Route::get('/dich-vu', [ServicesController::class,'index'])->name('show');
             
         // /services/detail/{slug}
-        Route::get('/services/detail/{id}', [ServicesController::class,'detail'])->name('detail');
+        Route::get('/dich-vu/chi-tiet/{id}', [ServicesController::class,'detail'])->name('detail');
     });
 
     Route::name('order_services.')->group(function () {
@@ -24,6 +23,7 @@ Route::name('frontend.')->group(function () {
         
         Route::post('/order/{id}', [OrderController::class,'addForm'])->name('addForm');
     });
+
     Route::name('contact_sendmail.')->group(function() {
         Route::get('/contact-form', [ContactController::class, 'contactForm'])->name('contact-form');
         Route::post('/contact-form', [ContactController::class, 'sendContact'])->name('contact.send');
