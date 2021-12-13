a861963dcfd3e8cbc46466fe7f58205a198dece6
your name <thienquan241096@gmail.com>
2021-12-13 23:33:39 +0700

    update

--- resources/views/backend/includes/navbar.blade.php
+++ resources/views/backend/includes/navbar.blade.php
@@ -1,8 +1,7 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="" class="brand-link">
-        {{-- {{ route('backend.admin.dashboard.show') }} --}}
+    <a href="backend.admin.dashboard.show" class="brand-link">
         <img src="{{ asset('backend/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">LupetCare</span>
     </a>
